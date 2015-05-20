417a75a5fb637b5f30d973b44ae5753bfbaffc96
Cirola2000 <cirobaroneto@gmail.com>
2015-05-20 15:17:22 +0200

    added a service starter

--- src/main/java/dynlod/linksets/StartService.java
+++ src/main/java/dynlod/linksets/StartService.java
@@ -9,39 +9,54 @@ import dynlod.mongodb.objects.DistributionMongoDBObject;
 import dynlod.mongodb.queries.Queries;
 
 // start service properly case service was killed in the middle of streaming
-public class StartService extends HttpServlet{
+public class StartService extends HttpServlet {
 
 	public StartService() {
 
-		ArrayList<DistributionMongoDBObject> d = new ArrayList<DistributionMongoDBObject>();
+		new Thread(new Runnable() {
 
-		// re-download distributions with "Downloading" status
-		ArrayList<String> q = Queries.getMongoDBObject(
-				DistributionMongoDBObject.COLLECTION_NAME,
-				DistributionMongoDBObject.STATUS,
-				DistributionMongoDBObject.STATUS_DOWNLOADING);
+			public void run() {
+				// TODO Auto-generated method stub
 
-		for (String s : q) {
-			DistributionMongoDBObject dist = new DistributionMongoDBObject(s);
-			dist.setStatus(DistributionMongoDBObject.STATUS_WAITING_TO_DOWNLOAD);
-			dist.updateObject(true);
-			d.add(dist);
-		}
+				ArrayList<DistributionMongoDBObject> d = new ArrayList<DistributionMongoDBObject>();
 
-		new Manager(d);
+				// re-download distributions with "Downloading" status
+				ArrayList<String> q = Queries.getMongoDBObject(
+						DistributionMongoDBObject.COLLECTION_NAME,
+						DistributionMongoDBObject.STATUS,
+						DistributionMongoDBObject.STATUS_DOWNLOADING);
+				System.out
+						.println("re-download distributions with \"Downloading\" status");
 
-		// download distributions with "STATUS_WAITING_TO_DOWNLOAD" status
-		q = Queries.getMongoDBObject(
-				DistributionMongoDBObject.COLLECTION_NAME,
-				DistributionMongoDBObject.STATUS,
-				DistributionMongoDBObject.STATUS_WAITING_TO_DOWNLOAD);
+				for (String s : q) {
+					DistributionMongoDBObject dist = new DistributionMongoDBObject(
+							s);
+					dist.setStatus(DistributionMongoDBObject.STATUS_WAITING_TO_DOWNLOAD);
+					dist.updateObject(true);
+					d.add(dist);
+				}
 
-		for (String s : q) {
-			DistributionMongoDBObject dist = new DistributionMongoDBObject(s);
-			d.add(dist);
-		}
+				new Manager(d);
 
-		new Manager(d);
+				// download distributions with "STATUS_WAITING_TO_DOWNLOAD"
+				// status
+				q = Queries.getMongoDBObject(
+						DistributionMongoDBObject.COLLECTION_NAME,
+						DistributionMongoDBObject.STATUS,
+						DistributionMongoDBObject.STATUS_WAITING_TO_DOWNLOAD);
+				System.out
+						.println("download distributions with \"STATUS_WAITING_TO_DOWNLOAD\" status");
+
+				for (String s : q) {
+					DistributionMongoDBObject dist = new DistributionMongoDBObject(
+							s);
+					d.add(dist);
+				}
+
+				new Manager(d);
+			}
+		}).start();
+		;
 
 	}
 
